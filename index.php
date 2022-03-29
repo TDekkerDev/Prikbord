8a8f0b7df21727ea90eb138bf2abfa3a0e4457bd
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-03-29 11:12:36 +0200

    h

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php include "php/inc/header.php"; ?>
+
 <?php include "php/inc/navbar.php"; ?>
 <?php include "php/inc/footer.php"; ?>
--- php/inc/header.php
+++ php/inc/header.php
@@ -10,4 +10,9 @@
     <title>prikbord</title>
 </head>
 <body>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
     
