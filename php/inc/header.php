23eb7513bb62df1b9e40626edf25ce5721bbc4e4
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-03-29 10:09:30 +0200

    ww

---
+++ php/inc/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/js/jquery-3.6.0.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <title>prikbord</title>
+</head>
+<body>
+    
