53f4753004717ff12fb347353c85b54bafc8fefc
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-03-30 13:04:33 +0200

    ww

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php include "php/inc/header.php"; ?>
-
 <?php include "php/inc/navbar.php"; ?>
+<?php include "php/tokengen.php"; ?>
+<form action="index.php" method="post">
+    <div class="form-group">
+        <label for="username">name</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Enter name">
+    </div>
+    <div class="form-group">
+        <label for="password">Email</label>
+        <input type="email" class="form-control" id="Email" name="Email" placeholder="Enter Email">
+    </div>
+    <div class="form-group">
+        <input type="text" class="form-control" id="token" name="token" hidden>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 <?php include "php/inc/footer.php"; ?>
---
+++ sql/prikbord.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE prikbord;
+use prikbord
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE `thomas` (
+                          `id` int(11) NOT NULL,
+                          `email` varchar(21) NOT NULL,
+                          `naam` varchar(21) NOT NULL,
+                          `token` varchar(25) NOT NULL,
+                          `boodschap` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ALTER TABLE `thomas`
+    ADD PRIMARY KEY (`id`);
+ALTER TABLE `thomas`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
